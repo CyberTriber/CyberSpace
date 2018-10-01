af7f8a4df90c53d4c7afe1c577f92573b940f42f
powered by coffee <31550401+CyberTriber@users.noreply.github.com>
2018-10-01 09:46:05 +0200

    main script
    run game with  `python3 CyberSpace.py`

---
+++ CyberSpace.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""
+  Discord RPG survival coop game by CyberCity dev team (https://discord.gg/NdrhvcF)
+  version: 0.1.0
+
+==============================================================
+"""
+
+from secrets.discord_secrets import *			# Import secret info for discord and firebase login
+from functions.discord_connect import *			# Import functions to connect discord
+
+
+def startBot():
+	client.run(BOT_TOKEN)
+
+if __name__ == '__main__':
+	startBot()
