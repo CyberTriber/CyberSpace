f4b846da5954c3a0cc03212a30595c143511840f
powered by coffee <31550401+CyberTriber@users.noreply.github.com>
2018-10-03 10:17:05 +0200

    added welcome message

--- variables/game_creator.py
+++ variables/game_creator.py
@@ -7,3 +7,19 @@ MAX_CHANNELS = 10
 MAX_USERS = 30
 CURR_CH = 0
 JSON_SERVERS = './servers.json'
+welcomeMSG = '''
+Select your class with `?class <number>`
+```
+  1. Captain
+  2. Soldier
+  3. Medic
+  4. Engineer
+  5. Chef
+```
+To start a game at least one of this class is required: Captain, Soldier
+but all class is needed to survive in dark space.
+
+If your team is ready, captain can start game with command `?start`
+'''
+
+
